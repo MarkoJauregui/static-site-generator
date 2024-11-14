d283e09769093063e326dcbb61f052779e106bff
MarkoJauregui <markojau@gmail.com>
2024-11-14 20:38:13 -0300

    Added image and link methods to node_splitter + unit tests

--- src/node_splitter.py
+++ src/node_splitter.py
@@ -1,4 +1,5 @@
 from textnode import TextNode, TextType
+from text_extraction import extract_markdown_images, extract_markdown_links
 
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
@@ -17,6 +18,67 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                 new_nodes.append(TextNode(part, TextType.TEXT))
             else:
                 new_nodes.append(TextNode(part, text_type))
+
+        result_nodes.extend(new_nodes)
+
+    return result_nodes
+
+
+def split_nodes_link(old_nodes):
+    result_nodes = []
+
+    for node in old_nodes:
+        current_text = node.text
+        new_nodes = []
+
+        while current_text:
+            link_and_url = extract_markdown_links(current_text)
+            if link_and_url:
+                link, url = link_and_url[0]
+                parts = current_text.split(f"[{link}]({url})", 1)
+
+                if parts[0]:
+                    new_nodes.append(TextNode(parts[0], TextType.TEXT))
+
+                new_nodes.append(TextNode(link, TextType.LINK, url))
+
+                current_text = parts[1]
+            else:
+                break
+
+        if current_text.strip():
+            new_nodes.append(TextNode(current_text, TextType.TEXT))
+
+        result_nodes.extend(new_nodes)
+
+    return result_nodes
+
+
+def split_nodes_image(old_nodes):
+    result_nodes = []
+
+    for node in old_nodes:
+        current_text = node.text
+        new_nodes = []
+
+        while current_text:
+            alt_and_url = extract_markdown_images(current_text)
+            if alt_and_url:
+                alt, url = alt_and_url[0]
+                parts = current_text.split(f"![{alt}]({url})", 1)
+
+                if parts[0]:
+                    new_nodes.append(TextNode(parts[0], TextType.TEXT))
+
+                new_nodes.append(TextNode(alt, TextType.IMAGE, url))
+
+                current_text = parts[1]
+
+            else:
+                break
+
+        if current_text.strip():
+            new_nodes.append(TextNode(current_text, TextType.TEXT))
 
         result_nodes.extend(new_nodes)
 
--- src/test_node_splitter.py
+++ src/test_node_splitter.py
@@ -1,6 +1,6 @@
 import unittest
 from textnode import TextNode, TextType
-from node_splitter import split_nodes_delimiter
+from node_splitter import split_nodes_delimiter, split_nodes_image, split_nodes_link
 
 
 class TestSplitNodesDelimiter(unittest.TestCase):
@@ -36,6 +36,117 @@ class TestSplitNodesDelimiter(unittest.TestCase):
         node = TextNode("This is **bold without closing", TextType.TEXT)
         with self.assertRaises(ValueError):
             split_nodes_delimiter([node], "**", TextType.BOLD)
+
+
+class TestSplitNodesImageAndLink(unittest.TestCase):
+
+    def test_split_nodes_link(self):
+        node = TextNode(
+            "Check this [link1](http://example.com) and [link2](http://another.com)",
+            TextType.TEXT,
+        )
+        expected = [
+            TextNode("Check this ", TextType.TEXT),
+            TextNode("link1", TextType.LINK, "http://example.com"),
+            TextNode(" and ", TextType.TEXT),
+            TextNode("link2", TextType.LINK, "http://another.com"),
+        ]
+        result = split_nodes_link([node])
+        self.assertEqual(result, expected)
+
+    def test_single_link(self):
+        node = TextNode("Visit [Boot.dev](https://www.boot.dev)", TextType.TEXT)
+        expected = [
+            TextNode("Visit ", TextType.TEXT),
+            TextNode("Boot.dev", TextType.LINK, "https://www.boot.dev"),
+        ]
+        result = split_nodes_link([node])
+        self.assertEqual(expected, result)
+
+    def test_multiple_links(self):
+        node = TextNode(
+            "Visit [Boot.dev](https://www.boot.dev) and [Youtube](https://www.youtube.com)",
+            TextType.TEXT,
+        )
+        expected = [
+            TextNode("Visit ", TextType.TEXT),
+            TextNode("Boot.dev", TextType.LINK, "https://www.boot.dev"),
+            TextNode(" and ", TextType.TEXT),
+            TextNode("Youtube", TextType.LINK, "https://www.youtube.com"),
+        ]
+        result = split_nodes_link([node])
+        self.assertEqual(expected, result)
+
+    def test_no_link(self):
+        node = TextNode("Don't go to boot.dev", TextType.TEXT)
+        expected = [TextNode("Don't go to boot.dev", TextType.TEXT)]
+        result = split_nodes_link([node])
+        self.assertEqual(expected, result)
+
+    def test_split_images(self):
+        node = TextNode(
+            "Here is an image: ![Alt text](https://example.com/image.jpg) and more text.",
+            TextType.TEXT,
+        )
+        expected = [
+            TextNode("Here is an image: ", TextType.TEXT),
+            TextNode("Alt text", TextType.IMAGE, "https://example.com/image.jpg"),
+            TextNode(" and more text.", TextType.TEXT),
+        ]
+        result = split_nodes_image([node])
+        self.assertEqual(expected, result)
+
+    def test_single_image(self):
+        node = TextNode(
+            "Here is an image: ![Alt text](https://example.com/image.jpg)",
+            TextType.TEXT,
+        )
+        expected = [
+            TextNode("Here is an image: ", TextType.TEXT),
+            TextNode("Alt text", TextType.IMAGE, "https://example.com/image.jpg"),
+        ]
+        result = split_nodes_image([node])
+        self.assertEqual(expected, result)
+
+    def test_multiple_images(self):
+        node = TextNode(
+            "Here is an image: ![Alt text](https://example.com/image.jpg) and another image ![Second Alt Text](https://example.com/image2.jpg)",
+            TextType.TEXT,
+        )
+        expected = [
+            TextNode("Here is an image: ", TextType.TEXT),
+            TextNode("Alt text", TextType.IMAGE, "https://example.com/image.jpg"),
+            TextNode(" and another image ", TextType.TEXT),
+            TextNode(
+                "Second Alt Text", TextType.IMAGE, "https://example.com/image2.jpg"
+            ),
+        ]
+        result = split_nodes_image([node])
+        self.assertEqual(expected, result)
+
+    def test_no_image(self):
+        node = TextNode("Here is no image", TextType.TEXT)
+        expected = [TextNode("Here is no image", TextType.TEXT)]
+        result = split_nodes_image([node])
+        self.assertEqual(expected, result)
+
+    def test_wrong_link(self):
+        node = TextNode("Here is a useless link [Boot.dev]", TextType.TEXT)
+        expected = [TextNode("Here is a useless link [Boot.dev]", TextType.TEXT)]
+        result = split_nodes_link([node])
+        self.assertEqual(expected, result)
+
+    def test_wrong_image(self):
+        node = TextNode(
+            "Here is a bad image (https://example.com/image.jpg)", TextType.TEXT
+        )
+        expected = [
+            TextNode(
+                "Here is a bad image (https://example.com/image.jpg)", TextType.TEXT
+            )
+        ]
+        result = split_nodes_image([node])
+        self.assertEqual(expected, result)
 
 
 if __name__ == "__main__":
